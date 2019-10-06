9149d48f8093b6bcbf1425774853fcf7eef8aa17
Felipe Paraizo <felipeparaizo@gmail.com>
2019-10-06 01:10:08 -0300

    defined operations to requests and start main function

---
+++ handlers/handlers_test.go
@@ -0,0 +1,47 @@
+package handlers
+
+import (
+	"database/sql"
+	"io"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+	"work-at-olist/storage"
+)
+
+var (
+	h  *Handler
+	DB *sql.DB
+)
+
+func makeRequest(t *testing.T, method string, url string, body io.Reader, header http.Header) *httptest.ResponseRecorder {
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if header != nil {
+		req.Header = header
+	}
+
+	var recorder = httptest.NewRecorder()
+	h.RecordsHandler(recorder, req)
+	h.BillsHandler(recorder, req)
+
+	return recorder
+}
+
+func init() {
+	logger := log.New(os.Stdout, "", log.LstdFlags|log.Lshortfile)
+
+	db, err := storage.NewDB("sqlite3", "./test.db")
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	DB = db.DB
+
+	db.InitSchema()
+	h = New(db, logger)
+}
